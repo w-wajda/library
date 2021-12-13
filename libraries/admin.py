3a62a92c0553c2a1e838b9b243d2f70d83190dbe
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2021-12-13 23:22:36 +0100

    new

--- libraries/admin.py
+++ libraries/admin.py
@@ -2,7 +2,43 @@ from django.contrib import admin
 from libraries.models import Author, Category, Publisher, Book
 
 
-admin.site.register(Author)
-admin.site.register(Category)
-admin.site.register(Publisher)
-admin.site.register(Book)
+class AuthorAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ("Author's data", {
+            'fields': ('name', 'surname')
+        }),
+        ('Additional info', {
+            'fields': ('date_birth',)
+        })
+    )
+    search_fields = ('name', 'surname')
+
+
+class PublisherAdmin(admin.ModelAdmin):
+    fields = ('name',)
+    search_fields = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ('name',)
+    search_fields = ('name',)
+
+
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = (
+      ('Basic data', {
+          'fields': ('title', 'author', 'description')
+      }),
+      ('Additional info', {
+          'fields': ('categories', ('publisher', 'publication_year'))
+      }),
+    )
+    list_display = ('title', 'author', 'publisher', 'publication_year')
+    search_fields = ('title', 'author', 'publisher')
+    list_filter = ('author', 'categories', 'publisher')
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Publisher, PublisherAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Book, BookAdmin)
