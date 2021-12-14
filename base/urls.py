75bafc94dd9538f900f853212033b94a77709fec
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2021-12-14 18:14:54 +0100

    new

--- base/urls.py
+++ base/urls.py
@@ -1,4 +1,4 @@
-"""library URL Configuration
+"""base URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -16,6 +16,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.urls import (
+    include,
+    path
+)
+from rest_framework import routers
+from libraries import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
